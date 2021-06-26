7e640da88c6a73ad5afc50013c7760816799bb7d
Jacob Doren <Jacob Doren>
2021-06-26 13:51:42 -0400

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <img src={logo} alt="logo" />
+        </nav>
+    )
+}
+
+export default Navbar
