43146ad431ecc2cf0730bdef35358f34923133ac
Jacob Doren <Jacob Doren>
2021-06-26 23:19:06 -0400

    added navbar background change on scroll past 80px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false)
+
+    const changeNavBackground = () => {
+       if(window.scrollY >= 80) {
+            setNavbar(true)
+        } else {
+            setNavbar(false)
+        } 
+    }
+
+    
+
+
+    window.addEventListener("scroll", changeNavBackground)
+    
     return (
-        <nav>
+        <nav className={navbar && "nav-set" }>
             <div className="nav-wrapper">
                 <h1>Home</h1>
                 <div className="links">
